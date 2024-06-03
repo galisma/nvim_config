553503434bde5a9911b58f45b09f514f8a36d825
Ismael Gallo <igallomunoz@proton.me>
2024-06-03 10:17:49 +0200

    configuracion añadida

---
+++ lua/config/settings.lua
@@ -0,0 +1,8 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.g.mapleader = " "
+vim.keymap.set('n', '<C-s>', '<cmd>:w<CR>')
+vim.bo.tabstop = 2
+vim.bo.softtabstop = 2
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 2
