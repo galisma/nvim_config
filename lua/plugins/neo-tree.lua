b47daa5ded3635b0b5f0ec103ea91ea86496e4a2
Galisma <igallomunoz@proton.me>
2024-06-05 14:54:55 +0200

    arreglos varios

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,19 +1,20 @@
- --[[
-require("neo-tree").setup({
-    source_selector = {
-        winbar = false,
-        statusline = false
-    }
-})
---]]
-
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+        require("neo-tree").setup({
+            source_selector = {
+                winbar = false,
+                statusline = true
+            }
+        })
+        vim.keymap.set('n', '<leader>n', ':Neotree toggle<CR>')
+    end
 }
+
